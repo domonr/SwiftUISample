26b7708d9dad1cacd4caeea5154d3a98ac34b036
Domon <domon@andfactory.co.jp>
2022-03-10 18:54:22 +0900

    add button and stack

--- SwiftUISample/SwiftUISample/ContentView.swift
+++ SwiftUISample/SwiftUISample/ContentView.swift
@@ -9,19 +9,36 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!\nHello, world!")
-            .fontWeight(.heavy)
-            .foregroundColor(.yellow)
-            .frame(width: nil)
-            .background(.red)
-            .lineLimit(nil)
-            .lineLimit(1)
-            .border(.yellow, width: 10)
-            .cornerRadius(200)
-            .padding()
-        Text("gghyyhgghyyhgghyyhgghyy")
-            .hiraginoW6(30)
-            .padding()
+        ZStack {
+            Color.yellow
+            VStack {
+                HStack(alignment: .bottom, spacing: 10) {
+                    Text("Hello, world!\nHello, world!")
+                        .fontWeight(.heavy)
+                        .foregroundColor(.yellow)
+                        .frame(width: nil)
+                        .background(.red)
+                        .lineLimit(nil)
+                        .lineLimit(1)
+                        .border(.yellow, width: 10)
+                        .cornerRadius(200)
+                        .padding()
+                    Text("gghyyhgghyyhgghyyhgghyy")
+                        .hiraginoW6(30)
+                        .padding()
+                }
+                HStack {
+                    Button("Hello") {
+                        print("Hello")
+                    }.padding()
+                    Button(action: {
+                        print("Hello")
+                    }, label: {
+                        Image(systemName: "star.fill")
+                    }).padding()
+                }
+            }
+        }
     }
 }
 
@@ -39,7 +56,7 @@ extension Text {
         let ctFont = CTFontCreateWithName("HiraginoSans-W6" as CFString, size, nil)
         let descent = CTFontGetDescent(ctFont)
         return self.font(.init(ctFont))
-        .baselineOffset(descent)
-        .offset(y: descent / 2)
+            .baselineOffset(descent)
+            .offset(y: descent / 2)
     }
 }
