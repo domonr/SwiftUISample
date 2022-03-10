1a2fbbb5f874ad10324495e5db9998463ed39646
Domon <domon@andfactory.co.jp>
2022-03-10 18:43:38 +0900

    Text Changed

--- SwiftUISample/SwiftUISample/ContentView.swift
+++ SwiftUISample/SwiftUISample/ContentView.swift
@@ -9,7 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
+        Text("Hello, world!\nHello, world!")
+            .fontWeight(.heavy)
+            .foregroundColor(.yellow)
+            .frame(width: nil)
+            .background(.red)
+            .lineLimit(nil)
+            .lineLimit(1)
+            .border(.yellow, width: 10)
+            .cornerRadius(200)
+            .padding()
+        Text("gghyyhgghyyhgghyyhgghyy")
+            .hiraginoW6(30)
             .padding()
     }
 }
@@ -19,5 +30,16 @@ struct ContentView_Previews: PreviewProvider {
         Group {
             ContentView()
         }
+    }
+}
+
+extension Text {
+    
+    func hiraginoW6(_ size: CGFloat) -> some View {
+        let ctFont = CTFontCreateWithName("HiraginoSans-W6" as CFString, size, nil)
+        let descent = CTFontGetDescent(ctFont)
+        return self.font(.init(ctFont))
+        .baselineOffset(descent)
+        .offset(y: descent / 2)
     }
 }
