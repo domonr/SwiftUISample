869be91f974816e35eee3735962225dd0d2d5f8f
Domon <domon@andfactory.co.jp>
2022-03-10 18:10:45 +0900

    Hello World

---
+++ SwiftUISample/SwiftUISample/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  SwiftUISample
+//
+//  Created by Domon on 2022/03/10.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        Group {
+            ContentView()
+        }
+    }
+}
---
+++ SwiftUISample/SwiftUISample/SwiftUISampleApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUISampleApp.swift
+//  SwiftUISample
+//
+//  Created by Domon on 2022/03/10.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUISampleApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
