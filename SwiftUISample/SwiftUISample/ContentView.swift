742a6345c9253c59242515c2385e0aac2a4c2950
Domon <domon@andfactory.co.jp>
2022-03-17 18:42:48 +0900

    lazy and scrollView

--- SwiftUISample/SwiftUISample/ContentView.swift
+++ SwiftUISample/SwiftUISample/ContentView.swift
@@ -8,34 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var data = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N"]
+    
     var body: some View {
         ZStack {
             Color.yellow
-            VStack {
-                HStack(alignment: .bottom, spacing: 10) {
-                    Text("Hello, world!\nHello, world!")
-                        .fontWeight(.heavy)
-                        .foregroundColor(.yellow)
-                        .frame(width: nil)
-                        .background(.red)
-                        .lineLimit(nil)
-                        .lineLimit(1)
-                        .border(.yellow, width: 10)
-                        .cornerRadius(200)
-                        .padding()
-                    Text("gghyyhgghyyhgghyyhgghyy")
-                        .hiraginoW6(30)
-                        .padding()
-                }
-                HStack {
-                    Button("Hello") {
-                        print("Hello")
-                    }.padding()
-                    Button(action: {
-                        print("Hello")
-                    }, label: {
-                        Image(systemName: "star.fill")
-                    }).padding()
+            ScrollView {
+                HStack(alignment: .top, spacing: 10) {
+                    LazyVStack {
+                        ForEach(data, id: \.self) { d in
+                            Text(d).onAppear {
+                                print(d + ": onAppear")
+                            }.onDisappear {
+                                print(d + ": onDisappear")
+                            }
+                        }.padding()
+                    }
+                    VStack {
+                        Text("Hello, world!\nHello, world!")
+                            .fontWeight(.heavy)
+                            .foregroundColor(.yellow)
+                            .frame(width: nil)
+                            .background(.red)
+                            .lineLimit(nil)
+                            .lineLimit(1)
+                            .border(.yellow, width: 10)
+                            .cornerRadius(200)
+                            .padding()
+                        Text("gghyyhgghyyhgghyyhgghyy")
+                            .hiraginoW6(30)
+                            .padding()
+                        Button("Hello") {
+                            print("Hello")
+                        }.padding()
+                        Button(action: {
+                            print("Hello")
+                        }, label: {
+                            Image(systemName: "star.fill")
+                        }).padding()
+                    }
                 }
             }
         }
@@ -59,4 +71,19 @@ extension Text {
             .baselineOffset(descent)
             .offset(y: descent / 2)
     }
+}
+
+struct HeaderView: View {
+    @State var viewData: DemoViewData
+    
+    var body: some View {
+        VStack {
+            Spacer()
+            Text("ID: \(viewData.id)")
+        }
+    }
+}
+
+struct DemoViewData {
+    var id: Int
 }
